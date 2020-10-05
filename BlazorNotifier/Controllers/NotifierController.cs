d9fc4c533d36eed6b8f68950129125f837d48a6b
APlatonenkov <platonenkov87@gmail.com>
2020-10-05 11:02:49 +0300

    fix namespace

--- BlazorNotifier/Controllers/NotifierController.cs
+++ BlazorNotifier/Controllers/NotifierController.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.IO;
 using System.Threading.Tasks;
-using BlazorNotificationTemplate.RealTimeApi;
 using BlazorNotifier.Classes;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SignalR;
