2a55fa0148b5431662f9b399648b9abd73f68e48
APlatonenkov <platonenkov87@gmail.com>
2020-10-06 18:09:56 +0300

    Debug уведомления для клиента, событие изменения статуса подключения к серверу

--- BlazorNotifier/Classes/NotifiMessage.cs
+++ BlazorNotifier/Classes/NotifiMessage.cs
@@ -5,7 +5,7 @@ namespace BlazorNotifier.Classes
     public class BlazorNotifierMessage
     {
         public string Title { get; set; }
-        public DateTime? Time { get; set; } = DateTime.Now;
+        public DateTime Time { get; set; } = DateTime.Now;
         public BlazorNotifierType Type { get; set; } = BlazorNotifierType.none;
         public bool IsPrivate { get; set; } = true;
         public string FromUserId { get; set; }
--- BlazorNotifier/Services/Implementations/BlazorNotifierClientService.cs
+++ BlazorNotifier/Services/Implementations/BlazorNotifierClientService.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Threading.Tasks;
 using BlazorNotifier.Classes;
 using Microsoft.AspNetCore.SignalR.Client;
 
@@ -8,27 +10,42 @@ namespace BlazorNotifier.Services.Implementations
 {
     public class BlazorNotifierClientService
     {
-        // Lets components receive change notifications
-        // Could have whatever granularity you want (more events, hierarchy...)
-        public event Action OnChange;
-        private void NotifyStateChanged() => OnChange?.Invoke();
+        #region События
 
+        /// <summary> Событие при изменениях в сервисе </summary>
+        public event Action OnChange;
+        /// <summary> Событие при изменении статуса подключения </summary>
+        public event Action OnConnectionStatusChange;
+        /// <summary> Событие при изменениях в сервисе </summary>
+        private void NotifyStateChanged() => OnChange?.Invoke();
+        /// <summary> Событие при изменении статуса подключения </summary>
+        private void NotifyConnectionChanged() => OnConnectionStatusChange?.Invoke();
+
+        #endregion
+
+        /// <summary> Конструктор с инициализатором сервиса </summary>
         public BlazorNotifierClientService()
         {
             ConnectToServerAsync();
         }
         #region Notification
-        public Dictionary<DateTime?, string> events = new Dictionary<DateTime?, string>();
-
-        string url = "https://localhost:44303/notificationhub";
-
-        public ObservableCollection<BlazorNotifierMessage> notifications = new ObservableCollection<BlazorNotifierMessage>();
+        /// <summary> Словарь событий время/сообщение </summary>
+        public Dictionary<DateTime?, string> Events = new Dictionary<DateTime?, string>();
+        /// <summary> адрес сервиса api </summary>
+        string Url = "https://localhost:44303/notificationhub";
+        /// <summary> Коллекция уведомительных сообщений </summary>
+        public ObservableCollection<BlazorNotifierMessage> Notifications = new ObservableCollection<BlazorNotifierMessage>();
+        /// <summary> хаб для подключения к api </summary>
         HubConnection _Connection = null;
-
+        /// <summary> Id клиента для отправки сообщений </summary>
         public string UserId => _Connection.ConnectionId;
-
+        /// <summary> стиль статуса подключения к сервису </summary>
         public string StatusColor { get; set; } = "background-color: red; width: 20px; height: 20px; border-radius: 50%";
+
+        #region Состояния подключения к сервису
+
         private bool _IsConnected;
+        /// <summary> состояния подключения к сервису </summary>
         public bool IsConnected
         {
             get => _IsConnected;
@@ -36,12 +53,18 @@ namespace BlazorNotifier.Services.Implementations
             {
                 _IsConnected = value;
                 StatusColor = value ? "background-color: green; width: 20px; height: 20px; border-radius: 50%" : "background-color: red; width: 20px; height: 20px; border-radius: 50%";
+                NotifyConnectionChanged();
                 NotifyStateChanged();
             }
         }
 
+        #endregion
+
+        #region Состояние канала передачи данных
+
         private string _ConnectionStatus = "Closed";
-        public string connectionStatus
+        /// <summary> Состояния канала передачи данных в тестовом виде </summary>
+        public string ConnectionStatus
         {
             get => _ConnectionStatus;
             set
@@ -51,49 +74,94 @@ namespace BlazorNotifier.Services.Implementations
             }
         }
 
+        #endregion
 
+        /// <summary> Запись событий </summary>
+        /// <param name="message">сообщение</param>
         void ShowNotification(BlazorNotifierMessage message)
         {
-            events.Add(message.Time, $"{message.Type}: {message.Title}");
+            Events.Add(message.Time, $"{message.Type}: {message.Title}");
             NotifyStateChanged();
         }
+
+        /// <summary> Подключение к серверу </summary>
         private async void ConnectToServerAsync()
         {
-            notifications.CollectionChanged += async (sender, e) =>
-            {
-                if (!(sender is ObservableCollection<BlazorNotifierMessage>))
-                {
-                    return;
-                }
+            //Добавляя новое сообщение в коллекцию - записываем сообщение в события
+            Notifications.CollectionChanged += OnNotificationsCollectionChanged;
 
-                foreach (BlazorNotifierMessage message in e.NewItems)
-                {
-                    ShowNotification(message);
-                }
-            };
             _Connection = new HubConnectionBuilder()
-                .WithUrl(url)
+                .WithUrl(Url)
                 .Build();
 
             await _Connection.StartAsync();
             IsConnected = true;
 
-            connectionStatus = "Connected";
+            ConnectionStatus = "Connected";
 
-            _Connection.Closed += async (s) =>
-            {
-                IsConnected = false;
-                connectionStatus = "Disconnected";
-                await _Connection.StartAsync();
-                IsConnected = true;
-                connectionStatus = "Connected";
-            };
+            //автоматически присоединяемся к серверу при разъединении
+            _Connection.Closed += OnConnectionClosed;
+            //выполняем при получении сообщений клиентом
+            _Connection.On<BlazorNotifierMessage>("notification", DoWhenClientGetNewMessage);
+        }
+        /// <summary>
+        /// Выполняем при получении сообщений клиентом
+        /// </summary>
+        /// <param name="message">сообщение</param>
+        private void DoWhenClientGetNewMessage(BlazorNotifierMessage message)
+        {
+            Notifications.Add(message);
+            NotifyStateChanged();
+        }
 
-            _Connection.On<BlazorNotifierMessage>("notification", m =>
+        /// <summary>
+        /// Автоматически присоединяемся к серверу при разъединении
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        private async Task OnConnectionClosed(Exception s)
+        {
+            IsConnected = false;
+            ConnectionStatus = "Disconnected";
+            await _Connection.StartAsync();
+            IsConnected = true;
+            ConnectionStatus = "Connected";
+        }
+        /// <summary>
+        /// Добавляя новое сообщение в коллекцию - записываем сообщение в события
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnNotificationsCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            if (!(sender is ObservableCollection<BlazorNotifierMessage>))
             {
-                notifications.Add(m);
-                NotifyStateChanged();
-            });
+                return;
+            }
+
+            foreach (BlazorNotifierMessage message in e.NewItems)
+            {
+                ShowNotification(message);
+            }
+        }
+
+        #endregion
+
+        #region Уведомления на клиенте
+
+        public void SendNotification(BlazorNotifierMessage message)
+        {
+            Notifications.Add(message);
+        }
+        public void SendNotification(string text, BlazorNotifierType type = BlazorNotifierType.Info)
+        {
+            if (type == BlazorNotifierType.Debug)
+            {
+                Console.WriteLine($"Debug: {text}");
+                return;
+            }
+
+            Notifications.Add(new BlazorNotifierMessage{Title = text, Type = type});
         }
 
         #endregion
